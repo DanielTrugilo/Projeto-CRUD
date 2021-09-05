c460d0506f5683e9c77d7b7ae135b9ced5b44f0d
Daniel Vitor de Azevedo Trugilo <dvat03@gmail.com>
2021-09-04 23:49:01 -0300

    Desenvolvimento do Front e API
    Criada a API que carrega a lista de produtos para o início do site, além da implementação da barra de busca que permite que eu busque por qualquer informação do item. Implementação do CSS no rodapé, topo, busca e listagem de produtos.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Titulo Bonito</h1>
+    <Buscar />
+    <Lista />
+  </section>
 </template>
 
 <script>
+import Buscar from "@/components/Buscar.vue";
+import Lista from "@/components/Lista.vue";
+
 export default {
   name: "Home",
+  components: {
+    Buscar,
+    Lista,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #84f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
